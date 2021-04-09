46b62006ae63eb9f2a326f7f2b86b1358361bb56
roliboy <roliboy@protonmail.com>
2021-04-09 12:58:20 +0300

    add steering_committee field on conference serializer

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -10,3 +10,12 @@ class IsAuthenticatedView(APIView):
 
     def get(self, request):
         return Response({"authenticated": True}, status=status.HTTP_200_OK)
+
+
+# class IsAuthenticatedView(APIView):
+#     def get(self, request):
+#         if isinstance(request.user, AnonymousUser):
+#             return Response({"authenticated": False}, status=status.HTTP_200_OK)
+#         else:
+#             return Response({"authenticated": True}, status=status.HTTP_200_OK)
+#
--- backend/conference/serializers.py
+++ backend/conference/serializers.py
@@ -1,9 +1,17 @@
-from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
+from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import ModelSerializer
 
 from conference.models import Conference
+from role.models import Role
 
 
-class ConferenceSerializer(HyperlinkedModelSerializer):
+class ConferenceSerializer(ModelSerializer):
+    steering_committee = SerializerMethodField()
+
     class Meta:
         model = Conference
-        fields = ['id', 'title', 'description', 'deadline']
+        fields = ['id', 'title', 'description', 'deadline', 'steering_committee']
+
+    def get_steering_committee(self, conference):
+        return map(lambda role: role.user.username,
+                   Role.objects.filter(role='steering committee', conference=conference))
