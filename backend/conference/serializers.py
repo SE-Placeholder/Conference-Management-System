f441794b9dfacc4fa5189bd5c77e0debd5967ec1
roliboy <roliboy@protonmail.com>
2021-04-11 17:54:14 +0300

    move role creation inside serializer

--- backend/conference/serializers.py
+++ backend/conference/serializers.py
@@ -12,6 +12,13 @@ class ConferenceSerializer(ModelSerializer):
         model = Conference
         fields = ['id', 'title', 'description', 'deadline', 'steering_committee']
 
+    def create(self, validated_data):
+        print(self.context['request'].user)
+        conference = super().create(validated_data)
+        role = Role(role='steering committee', conference=conference, user=self.context['request'].user)
+        role.save()
+        return conference
+
     def get_steering_committee(self, conference):
         return map(lambda role: role.user.username,
                    Role.objects.filter(role='steering committee', conference=conference))
--- backend/conference/views.py
+++ backend/conference/views.py
@@ -1,26 +1,10 @@
-from rest_framework import status, mixins
-from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, GenericAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet, ModelViewSet
 
 from conference.models import Conference
 from conference.serializers import ConferenceSerializer
-from role.models import Role
+
 
 class ConferenceViewSet(ModelViewSet):
     queryset = Conference.objects.all()
     serializer_class = ConferenceSerializer
     lookup_field = 'id'
-
-    def create(self, request):
-        serializer = self.get_serializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-        conference = serializer.save()
-        # TODO: don't hardcode this
-        role = Role(role='steering committee', conference=conference, user=self.request.user)
-        role.save()
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
