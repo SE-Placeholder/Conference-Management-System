c4eb13245ceb488543927503d6b3bc52c33716c4
roliboy <roliboy@protonmail.com>
2021-04-22 09:57:51 +0300

    permission class and serializer for conferences

--- backend/conference/serializers.py
+++ backend/conference/serializers.py
@@ -1,6 +1,8 @@
-from rest_framework.fields import SerializerMethodField
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import SerializerMethodField, ListField, JSONField
 from rest_framework.serializers import ModelSerializer
 
+from api.utils import get_user
 from conference.models import Conference
 from paper.models import Paper
 from paper.serializers import PaperSerializer
@@ -9,30 +11,67 @@ from role.serializers import UserSerializer
 
 
 class ConferenceSerializer(ModelSerializer):
-    steering_committee = SerializerMethodField()
+    steering_committee = JSONField(binary=True, write_only=True, required=False)
     papers = SerializerMethodField()
 
     class Meta:
         model = Conference
-        fields = [
-            'id',
-            'title',
-            'description',
-            'deadline',
-            'location',
-            'date',
-            'fee',
-            'steering_committee',
-            'papers'
-        ]
+        fields = ['id', 'title', 'description', 'deadline', 'location', 'date', 'fee', 'steering_committee', 'papers']
 
-    @staticmethod
-    def get_steering_committee(conference):
-        steering_committee = UserSerializer(
-            map(lambda role: role.user,
-                SteeringCommitteeRole.objects.filter(conference=conference)),
-            many=True)
-        return steering_committee.data
+    def create(self, validated_data):
+        steering_committee = [self.context['request'].user]
+        errors = []
+
+        for user_id in list(set(validated_data.pop('steering_committee', []))):
+            user = get_user(user_id)
+            if user:
+                steering_committee.append(user)
+            else:
+                errors.append(f'user {user_id} not found.')
+
+        if errors:
+            raise ValidationError({'steering_committee': errors})
+
+        conference = super().create(validated_data)
+
+        for user in steering_committee:
+            SteeringCommitteeRole.objects.create(
+                user=user,
+                conference=conference)
+
+        return conference
+
+    def update(self, instance, validated_data):
+        steering_committee = []
+        errors = []
+
+        for user_id in list(set(validated_data.pop('steering_committee', []))):
+            user = get_user(user_id)
+            if user:
+                steering_committee.append(user)
+            else:
+                errors.append(f'user {user_id} not found.')
+
+        if errors:
+            raise ValidationError({'steering_committee': errors})
+
+        conference = super().update(instance, validated_data)
+
+        if steering_committee:
+            for role in SteeringCommitteeRole.objects.filter(conference=conference):
+                role.delete()
+
+        for user in steering_committee:
+            SteeringCommitteeRole.objects.create(user=user, conference=conference)
+
+        return conference
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data['steering_committee'] = UserSerializer(
+            map(lambda role: role.user, SteeringCommitteeRole.objects.filter(conference=instance)),
+            many=True).data
+        return data
 
     @staticmethod
     def get_papers(conference):
--- backend/conference/views.py
+++ backend/conference/views.py
@@ -1,82 +1,49 @@
-from django.contrib.auth.models import User
 from rest_framework import status, permissions
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, AllowAny, BasePermission, SAFE_METHODS
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ViewSet, ModelViewSet
 
 from conference.models import Conference
 from conference.serializers import ConferenceSerializer
-from role.models import SteeringCommitteeRole, ListenerRole
-from role.serializers import UserSerializer
+from role.models import ListenerRole, SteeringCommitteeRole
 
 
-# TODO: remove duplicate users from input
-# TODO: helper methods to reduce code duplication
+# create conference: allow authenticated users
+# list conferences: allow any
+# update conference: allow only users with steering committee role for that conference
+# retrieve conference: allow any
+class ConferencePermissions(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+        if not bool(request.user and request.user.is_authenticated):
+            return False
+        return SteeringCommitteeRole.objects.filter(user=request.user, conference=obj).exists()
+
+
 class ConferenceViewSet(ModelViewSet):
     queryset = Conference.objects.all()
     serializer_class = ConferenceSerializer
+    permission_classes = [ConferencePermissions]
     lookup_field = 'id'
-
-    def get_permissions(self):
-        return [AllowAny()] if self.action in ['list', 'retrieve'] else [IsAuthenticated()]
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        conference = serializer.save()
-
-        SteeringCommitteeRole.objects.create(
-            conference=conference,
-            user=request.user)
-
-        response = serializer.data
-        response['steering_committee'] = UserSerializer(
-            [request.user],
-            many=True).data
-        return Response(response, status=status.HTTP_201_CREATED)
-
-    # TODO: allow only conference steering committee members to update conference
-    def partial_update(self, request, *args, **kwargs):
-        steering_committee = []
-        errors = []
-        for username in request.data.get('steering_committee', []):
-            try:
-                steering_committee.append(User.objects.get(username=username))
-            except User.DoesNotExist:
-                errors.append(f'user {username} not found.')
-
-        if len(errors) != 0:
-            return Response({'steering_committee': errors}, status=status.HTTP_400_BAD_REQUEST)
-
-        serializer = self.get_serializer(self.get_object(), data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        conference = serializer.save()
-
-        if 'steering_committee' in request.data:
-            for role in SteeringCommitteeRole.objects.filter(conference=conference):
-                role.delete()
-
-        for user in steering_committee:
-            SteeringCommitteeRole.objects.create(
-                user=user,
-                conference=conference)
-
-        response = serializer.data
-        response['steering_committee'] = UserSerializer(
-            steering_committee,
-            many=True).data
-        return Response(response, status=status.HTTP_200_OK)
 
 
 class JoinConferenceView(APIView):
     permission_classes = [IsAuthenticated]
+
     def post(self, request, id):
         try:
             conference = Conference.objects.get(id=id)
 
             if ListenerRole.objects.filter(user=request.user, conference=conference).exists():
-                return Response({'status': 'Already registered for this conference'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'status': 'Already registered for this conference'},
+                                status=status.HTTP_400_BAD_REQUEST)
 
             ListenerRole.objects.create(
                 user=request.user,
